d33a2bef44e25c9a41bbddb0e441903640c92d03
ixtears23 <ixtears23@icloud.com>
2023-05-28 19:28:01 +0900

    프로젝트 생성

---
+++ src/main/java/junseok/snr/heachi01/Heachi01Application.java
@@ -0,0 +1,13 @@
+package junseok.snr.heachi01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Heachi01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Heachi01Application.class, args);
+    }
+
+}
---
+++ src/test/java/junseok/snr/heachi01/Heachi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package junseok.snr.heachi01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Heachi01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
