5ed1196df324a1330e65e874ad8f553b91d3a51f
ixtears23 <ixtears23@icloud.com>
2023-05-30 18:41:38 +0900

    이더리움 지갑 도메인 추가

---
+++ src/main/java/junseok/snr/heachi01/domain/EthereumWallet.java
@@ -0,0 +1,28 @@
+package junseok.snr.heachi01.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.web3j.crypto.*;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+
+@Getter
+@ToString
+public class EthereumWallet {
+    private final String publicKey;
+    private final String privateKey;
+    private final String address;
+
+    public EthereumWallet(final String password) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, CipherException {
+        ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+        Credentials credentials = Credentials.create(ecKeyPair);
+        WalletFile wallet = Wallet.createStandard(password, ecKeyPair);
+        privateKey = credentials.getEcKeyPair().getPrivateKey().toString(16);
+        publicKey = credentials.getEcKeyPair().getPublicKey().toString(16);
+        address = wallet.getAddress();
+    }
+
+}
