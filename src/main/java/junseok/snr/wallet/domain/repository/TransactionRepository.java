9a0e67c774e5389d40633e00d849bd867f2ee97f
ixtears23 <ixtears23@icloud.com>
2023-06-11 13:23:50 +0900

    [refactor] domain repository 에 메서드가 추가

--- src/main/java/junseok/snr/wallet/domain/repository/TransactionRepository.java
+++ src/main/java/junseok/snr/wallet/domain/repository/TransactionRepository.java
@@ -1,4 +1,13 @@
 package junseok.snr.wallet.domain.repository;
 
+import junseok.snr.wallet.domain.model.Transaction;
+import junseok.snr.wallet.domain.model.TransactionStatus;
+import junseok.snr.wallet.domain.model.TransactionType;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface TransactionRepository {
+    Transaction findFirstByTransactionHashAndType(String transactionHash, TransactionType transactionType);
+    List<Transaction> findLatestTransactions(@Param("status") TransactionStatus status);
 }
--- src/main/java/junseok/snr/wallet/domain/repository/WalletRepository.java
+++ src/main/java/junseok/snr/wallet/domain/repository/WalletRepository.java
@@ -1,4 +1,7 @@
 package junseok.snr.wallet.domain.repository;
 
+import junseok.snr.wallet.domain.model.Wallet;
+
 public interface WalletRepository {
+    Wallet findByAddress(String address);
 }
